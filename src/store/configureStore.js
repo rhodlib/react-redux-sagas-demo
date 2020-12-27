8a0a06dbf1518ad61b1c73f1fd154fa60efcf3af
Rodolfo Talibs <rhodlib@gmail.com>
2020-12-27 18:22:18 -0300

    redux-saga, configuring redux-saga

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,16 @@
-import {createStore} from 'redux';
+import {compose, createStore, applyMiddleware} from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import createRootReducer from './reducers';
+import rootSaga from '../sagas/rootSaga';
 
 export default function configureStore({initialState}){
+    const componseEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const sagaMiddleware = createSagaMiddleware();
+
     const store = createStore(
         createRootReducer(),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        componseEnhancers(applyMiddleware(sagaMiddleware))
     );
+    sagaMiddleware.run(rootSaga);
     return {store};
 }
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
-import counterReducer, {NAME as counterName} from '../features/counter'; //DUCKS!!!
+import counterReducer, {NAME as counterName} from '../features/counter';
+import usersReducer, {NAME as usersName} from '../features/users'
 
 export default () => combineReducers({
-    [counterName]: counterReducer
+    [counterName]: counterReducer,
+    [usersName]: usersReducer
 });
